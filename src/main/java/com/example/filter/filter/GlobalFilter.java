78ce2ac66d5e68b5719dd08d9d19c3e3ee6fd52c
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-05-25 23:33:30 +0900

    feat : filter

--- src/main/java/com/example/filter/filter/GlobalFilter.java
+++ src/main/java/com/example/filter/filter/GlobalFilter.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,18 +18,24 @@ public class GlobalFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         //전처리
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        ContentCachingRequestWrapper httpServletRequest = new ContentCachingRequestWrapper((HttpServletRequest) request);
+        ContentCachingResponseWrapper httpServletResponse = new ContentCachingResponseWrapper((HttpServletResponse) response);
 
         String url = httpServletRequest.getRequestURI();
 
-        BufferedReader br = httpServletRequest.getReader();
 
-        br.lines().forEach(line -> {
-            log.info("line : {}", line);
-        });
-        chain.doFilter(httpServletRequest,httpServletResponse);
+        chain.doFilter(httpServletRequest, httpServletResponse);
 
         //후처리
+        //req
+        String reqContent = new String(httpServletRequest.getContentAsByteArray());
+        log.info("response url : {}, responseBody", url, reqContent);
+
+        String resContent = new String(httpServletResponse.getContentAsByteArray());
+        int httpStatus = httpServletResponse.getStatus();
+
+        httpServletResponse.copyBodyToResponse();
+
+        log.info("response status : {}, responseBody", httpStatus, resContent);
     }
 }
